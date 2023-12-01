65e8d0d9b985be644d80d1b16388d8e12d6a0fd3
dmsuh <susuhinin@yandex.ru>
2023-12-01 13:27:21 +0300

    fbx add

---
+++ Assets/POI/Prefabs/Prefab/checker/chekerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chekerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject DefState, ErrorState, TrueState;
+    public Animator animator;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
